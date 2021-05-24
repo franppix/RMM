8c69054ab716362da9b4c49fa59110c4fc65b927
franppix <45703702+franppix@users.noreply.github.com>
2021-05-24 18:02:02 -0500

    Delete customer_202105241753.sql

--- SQL/customer_202105241753.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO rmm_services.customer (id_number,address,name,nationality,phone_number) VALUES 
-('E126','dirección 1','Printech','USA','098784')
-,('F126','dirección 2','ItemSale','Italia','098985')
-,('H126','dirección 3','Super','España','098111')
-,('P126','dirección','MieleSoft','Ecuador','098457')
-;
