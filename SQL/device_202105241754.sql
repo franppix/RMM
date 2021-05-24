aeac41511d1cc1ef1f82ffa4c9250989736e425b
franppix <45703702+franppix@users.noreply.github.com>
2021-05-24 18:02:14 -0500

    Delete device_202105241754.sql

--- SQL/device_202105241754.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO rmm_services.device (id,date_create,system_name,`type`,customer_id) VALUES 
-('LP130','2021-05-23','MAC124',2,'E126')
-,('LP131','2021-05-23','WINDOWS INFO',1,'E126')
-,('M1457','2021-05-23','Local Machine Mod',2,'F126')
-,('M1458','2021-05-23','Local Machine',2,'F126')
-,('M1459','2021-05-23','Local Machine',2,'F126')
-,('W1453','2021-05-23','Server Machine',1,'F126')
-,('W1457','2021-05-24','Local Machine Win',0,'F126')
-;
