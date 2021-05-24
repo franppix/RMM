d568aca22dd21a95f79a9db26ee50f8893b665a7
franppix <45703702+franppix@users.noreply.github.com>
2021-05-24 18:01:32 -0500

    Add files via upload

---
+++ SQL/service_202105241753.sql
@@ -0,0 +1,6 @@
+INSERT INTO rmm_services.service (id,service_name,description) VALUES 
+('ANT','ANTIVIRUS','To have antivirus in their devices.')
+,('CLO','CLOUDBERRY','To backup data in their devices.')
+,('PSA','PSA','Ticketing system for alerts in their devices.')
+,('TEV','TEAMVIEWER','Remote connection to devices.')
+;
---
+++ SQL/service_cost_202105241754.sql
@@ -0,0 +1,7 @@
+INSERT INTO rmm_services.service_cost (service_id,`system`,cost) VALUES 
+('ANT',1,5.0)
+,('ANT',2,7.0)
+,('CLO',0,3.0)
+,('PSA',0,2.0)
+,('TEV',0,1.0)
+;
